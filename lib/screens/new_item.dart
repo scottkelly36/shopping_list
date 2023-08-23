2d36fc98066bd1ade9be2ac1267fdfa7b3afe84c
scott kelly <scott.kelly@crtr.co.uk>
2023-08-23 16:40:39 +0100

    adds input form

--- lib/screens/new_item.dart
+++ lib/screens/new_item.dart
@@ -1,4 +1,8 @@
+//  packages
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+//  models
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -14,8 +18,52 @@ class _NewItemState extends State<NewItem> {
       appBar: AppBar(title: const Text('Add a new Item')),
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: Text('The Form')
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(label: Text('Name')),
+                validator: (value) {
+                  return 'Text';
+                },
+              ),
+              const SizedBox(height: 8),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(label: Text('Quantity')),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButton(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(width: 6),
+                              Text(category.value.title)
+                            ],
+                          ),
+                        ),
+                    ], onChanged: (value) {}),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
+      ),
     );
   }
 }

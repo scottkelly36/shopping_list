21ce62d6d446be3f3ac90610f7afa0289e39d4df
scott kelly <scott.kelly@crtr.co.uk>
2023-08-23 16:41:45 +0100

    corrects bugs

--- lib/screens/new_item.dart
+++ lib/screens/new_item.dart
@@ -1,21 +1,23 @@
-//  packages
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-//  models
+
 import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
 
   @override
-  _NewItemState createState() => _NewItemState();
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
 }
 
 class _NewItemState extends State<NewItem> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Add a new Item')),
+      appBar: AppBar(
+        title: const Text('Add a new item'),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
@@ -23,43 +25,49 @@ class _NewItemState extends State<NewItem> {
             children: [
               TextFormField(
                 maxLength: 50,
-                decoration: const InputDecoration(label: Text('Name')),
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
                 validator: (value) {
-                  return 'Text';
+                  return 'Demo...';
                 },
-              ),
-              const SizedBox(height: 8),
+              ), // instead of TextField()
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Expanded(
                     child: TextFormField(
-                      decoration: const InputDecoration(label: Text('Quantity')),
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
                       initialValue: '1',
                     ),
                   ),
                   const SizedBox(width: 8),
                   Expanded(
-                    child: DropdownButton(items: [
-                      for (final category in categories.entries)
-                        DropdownMenuItem(
-                          value: category.value,
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 16,
-                                height: 16,
-                                color: category.value.color,
-                              ),
-                              const SizedBox(width: 6),
-                              Text(category.value.title)
-                            ],
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(category.value.title),
+                              ],
+                            ),
                           ),
-                        ),
-                    ], onChanged: (value) {}),
-                  )
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
